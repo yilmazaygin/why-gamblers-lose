546712921fbc5ff26683638cdd176bfb884cc196
yilmazaygin <yilmazaygin@gmail.com>
2024-12-01 23:18:31 +0300

    new ideas

--- src/server/tryfile.py
+++ src/server/tryfile.py
@@ -1,14 +1,15 @@
-new_bet = {"Bet Amount": 50, 
-       "Bet Type": 0,
-       }
-total_bet = {"Balance Before Bet": 0,
-             "Overall Bet Amount": 0,
-             "Balance After Bet": 0,
-             "Overall Bet Condition": 0,
-             "bets": [[new_bet]]
-             }
-# A player can bet on multiple places on the board
-# Bet type is the type of bet the player is making
+# A player can bet on multiple places on board
+# So we need a total bet amount for each player
+initial_bet = {"Bet Amount":0, "Bet Type":0, "Bet Placement":0}
+total_bet = {"Total Bet Amount":0, "Balance Before Bets":0, "Balance After Bets":0, "Overall Profit":0, "Bet Condition":0}
 
+player_balance = 1000
 
-bet_history = []
+example_initial_bets = [{"Bet Amount":100, "Bet Type":"Red/Black", "Bet Placement":"Red"}, 
+                {"Bet Amount":100, "Bet Type":"Even/Odd", "Bet Placement":"Even"}]
+
+example_total_bet = {"Total Bet Amount": example_initial_bets[0]["Bet Amount"] + example_initial_bets[1]["Bet Amount"],
+                     "Balance Before Bets": player_balance, "Balance After Bets":player_balance - example_total_bet["Total Bet Amount"],
+                    "Overall Profit": 0, "Bet Condition": False}
+
+# Bu sadece aklıma gelenleri not almam, düzenlenecek.!!!!
