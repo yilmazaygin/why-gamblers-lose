09e3fd6ad13d8f36ee373db7d03c324268bb8b7b
yilmazaygin <yilmazaygin@gmail.com>
2024-12-01 23:18:06 +0300

    new ideas

--- src/server/tryfile.py
+++ src/server/tryfile.py
@@ -1,1 +1,14 @@
+new_bet = {"Bet Amount": 50, 
+       "Bet Type": 0,
+       }
+total_bet = {"Balance Before Bet": 0,
+             "Overall Bet Amount": 0,
+             "Balance After Bet": 0,
+             "Overall Bet Condition": 0,
+             "bets": [[new_bet]]
+             }
+# A player can bet on multiple places on the board
+# Bet type is the type of bet the player is making
 
+
+bet_history = []
